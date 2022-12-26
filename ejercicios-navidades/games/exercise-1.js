f021d5b844bdea59f36c1c14dbde74a704b87fac
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-26 21:42:01 +0100

    STYLE: games.exercise-1 - cleaning the code & colouring commented zones to set my progress

--- ejercicios-navidades/games/exercise-1.js
+++ ejercicios-navidades/games/exercise-1.js
@@ -78,26 +78,32 @@ let cardArray = [
 //*  1.  Pintar un tablero con las doce imágenes del array y el `src` apuntando al svg de `public/exercise/universe.svg`. 
 // * Ten en cuenta que como es un juego de parejas, las imágenes y nombres están repetidas en el array.
 
-//  2. Añadir a cada una de las imágenes un evento click para comprobar si la jugada realizada por el usuario es correcta.
-//  Para ello, tendremos que guardar cada dos click en tarjetas que haga el usuario y comparar uno con otro siguiendo
-//   las siguientes condiciones:
-//     1. Si el usuario hace click en la misma tarjeta dos veces, la jugada no es valida.
-//     2. Si el usuario hace click en una tarjeta previamente validada, la jugada no es valida.
-//     3. Si el usuario hace click en dos tarjetas con el nombre diferente, la jugada no es valida.
-//     4. Si ninguna de estas condiciones se cumple y, el usuario hace click en dos tarjetas con diferentes nombres. la
-//      jugada será validada y tendremos que almacenar las tarjetas para posteriores comprobaciones. Además, el ``src
-//      `` de la imagen debería de cambiar a ``public/exercise-1/tick.svg``.
+
+//!  2. Añadir a cada una de las imágenes un evento click para comprobar si la jugada realizada por el usuario es correcta.
+//!  Para ello, tendremos que guardar cada dos click en tarjetas que haga el usuario y comparar uno con otro siguiendo
+//!   las siguientes condiciones:
+
+//!     1. Si el usuario hace click en la misma tarjeta dos veces, la jugada no es valida.
+//!     2. Si el usuario hace click en una tarjeta previamente validada, la jugada no es valida.
+//!     3. Si el usuario hace click en dos tarjetas con el nombre diferente, la jugada no es valida.
+//!     4. Si ninguna de estas condiciones se cumple y, el usuario hace click en dos tarjetas con diferentes nombres. la
+//!      jugada será validada y tendremos que almacenar las tarjetas para posteriores comprobaciones. Además, el ``src
+//!      `` de la imagen debería de cambiar a ``public/exercise-1/tick.svg``.
+
 
 //  3. Por cada una de las jugadas deberemos validar el score y sumar 1 por cada jugada validada correctamente al span
 //   de ```score```. Si el
 //   usuario ha resuelto el juego completo, muestralé un mensaje de felicitaciones.
 
+
 //  4. Por último, para medir la calidad del juego, podríamos crear un contador de intentos e introducir el numero de
 //   intentos que el usuario ha llevado a cabo en el span de ``attempts``. Realmente este punto es bastante fácil, suma
 //    simplemente uno por cada vez que hagas una comprobación de jugada.
 
+
 // Si has cumplido todas las condiciones...Enhorabuena! tienes un juego de memoría bastante resultón! Además si te
 //  interesa subir la dificultad no tienes más que hacer el array más grande y meter más imagenes...¿te atreves? :D
+
 
 function createMemory (){
 
