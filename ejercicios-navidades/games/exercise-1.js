d9c7457d486636dc6051a4607b1faf21ab80fd71
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-29 22:48:12 +0100

    REF: Game #1 - Cleaning the code of commented code

--- ejercicios-navidades/games/exercise-1.js
+++ ejercicios-navidades/games/exercise-1.js
@@ -79,30 +79,32 @@ let cardArray = [
 // * Ten en cuenta que como es un juego de parejas, las imágenes y nombres están repetidas en el array.
 
 
-//!  2. Añadir a cada una de las imágenes un evento click para comprobar si la jugada realizada por el usuario es correcta.
-//!  Para ello, tendremos que guardar cada dos click en tarjetas que haga el usuario y comparar uno con otro siguiendo
-//!   las siguientes condiciones:
+//*  2. Añadir a cada una de las imágenes un evento click para comprobar si la jugada realizada por el usuario es correcta.
+//*  Para ello, tendremos que guardar cada dos click en tarjetas que haga el usuario y comparar uno con otro siguiendo
+//*   las siguientes condiciones:
 
-//!     1. Si el usuario hace click en la misma tarjeta dos veces, la jugada no es valida.
-//!     2. Si el usuario hace click en una tarjeta previamente validada, la jugada no es valida.
-//!     3. Si el usuario hace click en dos tarjetas con el nombre diferente, la jugada no es valida.
-//!     4. Si ninguna de estas condiciones se cumple y, el usuario hace click en dos tarjetas con diferentes nombres. la
-//!      jugada será validada y tendremos que almacenar las tarjetas para posteriores comprobaciones. Además, el ``src
-//!      `` de la imagen debería de cambiar a ``public/exercise-1/tick.svg``.
-
-
-//  3. Por cada una de las jugadas deberemos validar el score y sumar 1 por cada jugada validada correctamente al span
-//   de ```score```. Si el
-//   usuario ha resuelto el juego completo, muestralé un mensaje de felicitaciones.
+//*     1. Si el usuario hace click en la misma tarjeta dos veces, la jugada no es valida.
+//*     2. Si el usuario hace click en una tarjeta previamente validada, la jugada no es valida.
+//*     3. Si el usuario hace click en dos tarjetas con el nombre diferente, la jugada no es valida.
+//*     4. Si ninguna de estas condiciones se cumple y, el usuario hace click en dos tarjetas con diferentes nombres. la
+//*      jugada será validada y tendremos que almacenar las tarjetas para posteriores comprobaciones. Además, el ``src
+//*      `` de la imagen debería de cambiar a ``public/exercise-1/tick.svg``.
 
 
-//  4. Por último, para medir la calidad del juego, podríamos crear un contador de intentos e introducir el numero de
-//   intentos que el usuario ha llevado a cabo en el span de ``attempts``. Realmente este punto es bastante fácil, suma
-//    simplemente uno por cada vez que hagas una comprobación de jugada.
+//*  3. Por cada una de las jugadas deberemos validar el score y sumar 1 por cada jugada validada correctamente al span
+//*   de ```score```. Si el
+//*   usuario ha resuelto el juego completo, muestralé un mensaje de felicitaciones.
 
 
-// Si has cumplido todas las condiciones...Enhorabuena! tienes un juego de memoría bastante resultón! Además si te
-//  interesa subir la dificultad no tienes más que hacer el array más grande y meter más imagenes...¿te atreves? :D
+//*  4. Por último, para medir la calidad del juego, podríamos crear un contador de intentos e introducir el numero de
+//*   intentos que el usuario ha llevado a cabo en el span de ``attempts``. Realmente este punto es bastante fácil, suma
+//*    simplemente uno por cada vez que hagas una comprobación de jugada.
+
+
+//* Si has cumplido todas las condiciones...Enhorabuena! tienes un juego de memoría bastante resultón! Además si te
+//*  interesa subir la dificultad no tienes más que hacer el array más grande y meter más imagenes...¿te atreves? :D
+
+
 
 function createMemory (){
 
@@ -164,7 +166,6 @@ function createMemory (){
 };
 
 // creamos la función que haga invisible el reverso y así ver la carta
-  
 function showPlanet(event) {
 
     // hacemos visible el planeta seleccionado
@@ -269,6 +270,7 @@ function handler(e) {
     e.preventDefault();
 };
 
+// función que hace que al acabar la partida aparezcan los DIV con la enhorabuena y la info de la partida
 function finished () {
     const victory$$ = document.querySelector(".victoryDiv");
     victory$$.classList.add("finished");
@@ -278,7 +280,6 @@ function finished () {
 }
 
 //creamos el botón de RESET para iniciar de nuevo la partida, le damos atributos y lo insertamos a la web
-
 const reset$$ = document.createElement("button");
 reset$$.textContent = "RESET";
 reset$$.setAttribute("onclick","document.location.reload()");
